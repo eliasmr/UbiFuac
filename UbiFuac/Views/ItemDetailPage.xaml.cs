284515628841330d75ec6b6674ab5d7fa09550a1
elias.medina <elias.medina@fuac.edu.com>
2020-08-17 07:47:18 -0500

    remote commit proyecto ubicacion fuac

---
+++ UbiFuac/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel;
+using Xamarin.Forms;
+using UbiFuac.ViewModels;
+
+namespace UbiFuac.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
